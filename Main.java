d81c6abebe3113d8ad84cb21de4c073d6657b095
SiriGlück <siriluck.rattanapong@gmail.com>
2025-10-18 13:47:36 +0200

    GridLayout FlowLayout JLayerPane and NewWindow

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 import javax.swing.*;
-import java.awt.*;
+import javax.swing.border.Border;
+import java.awt.*; // AWT - Abstract Window Toolkit
 // import javax.swing.border.Border;
 
 public class Main {
@@ -19,7 +20,7 @@ public class Main {
         frame.getContentPane().setBackground(new Color(123,50,250)); // change color of background
          */
         MyFrame myFrame = new MyFrame(); // create an instance
-        //myFrame.setLayout(null);
+        // myFrame.setLayout(null); // need to bound, otherwise see nothing
 
         // ## JButton 1/3
         /*
@@ -80,56 +81,19 @@ public class Main {
         */
 
         // ## Border Layout
-        myFrame.setLayout(new BorderLayout(10,10));
+        myFrame.add(new BorderLayoutExample());
 
-        JPanel panel1 = new JPanel();
-        JPanel panel2 = new JPanel();
-        JPanel panel3 = new JPanel();
-        JPanel panel4 = new JPanel();
-        JPanel panel5 = new JPanel();
+        // ## Grid Layout
+        // myFrame.add(new GridLayoutExample());
 
-        panel1.setBackground(Color.red);
-        panel2.setBackground(Color.yellow);
-        panel3.setBackground(Color.blue);
-        panel4.setBackground(Color.green);
-        panel5.setBackground(Color.white);
+        // ## Flow Layout
+        // myFrame.add(new FlowLayoutExample(), BorderLayout.SOUTH);
 
-        myFrame.add(panel1, BorderLayout.NORTH);
-        myFrame.add(panel2, BorderLayout.SOUTH);
-        myFrame.add(panel3, BorderLayout.EAST);
-        myFrame.add(panel4, BorderLayout.WEST);
-        myFrame.add(panel5, BorderLayout.CENTER);
+        // ## JLayeredPane - container provides depth/z-index
+        // myFrame.add(new JLayeredPaneExample());
 
-        panel1.setPreferredSize(new Dimension(100,100));
-        panel2.setPreferredSize(new Dimension(100,100));
-        panel3.setPreferredSize(new Dimension(100,100));
-        panel4.setPreferredSize(new Dimension(100,100));
-        panel5.setPreferredSize(new Dimension(100,100));
-
-        // -- sub layout
-        panel5.setLayout(new BorderLayout(10,10));
-
-        JPanel panel6 = new JPanel();
-        JPanel panel7 = new JPanel();
-        JPanel panel8 = new JPanel();
-        JPanel panel9 = new JPanel();
-
-        panel6.setBackground(Color.black);
-        panel7.setBackground(Color.gray);
-        panel8.setBackground(Color.lightGray);
-        panel9.setBackground(Color.darkGray);
-
-        panel6.setPreferredSize(new Dimension(50,50));
-        panel7.setPreferredSize(new Dimension(50,50));
-        panel8.setPreferredSize(new Dimension(50,50));
-        panel9.setPreferredSize(new Dimension(50,50));
-
-        panel5.add(panel6, BorderLayout.NORTH);
-        panel5.add(panel7, BorderLayout.SOUTH);
-        panel5.add(panel8, BorderLayout.EAST);
-        panel5.add(panel9, BorderLayout.WEST);
-
-
+        // ## NewWindow
+         myFrame.add(new NewWindowLaunchPage());
 
 
         myFrame.setVisible(true); // make frame visible
